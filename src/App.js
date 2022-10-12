26b6808cf3061b97aedf74230e8eeed56fe52d51
Alex Sherbin <contact.alexsherbin@gmail.com>
2022-10-12 13:36:47 -0400

    Task-2.1; Created carousels markup, added state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+// Components
+import Carousel from 'components/Carousel';
+
 // Styles
 import 'assets/styles/main.css';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <div className='container'>
+        <Carousel />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,22 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import styles from './Carousel.module.css';
 
-const dummyData = [1, 2, 3, 4, 5, 6, 7, 8];
+const dummyData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Carousel = () => {
+  const [translateValue, setTranslateValue] = useState(0);
+  const [translateIncrement, setTranslateIncrement] = useState(100);
+  const ref = useRef();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const itemsPerScreen = window
+        .getComputedStyle(ref.current)
+        .getPropertyValue('--items-per-screen');
+
+      setTranslateIncrement(100 / itemsPerScreen);
+      setTranslateValue((translateValue * 100) / itemsPerScreen);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleTranslateChange = (e, translateIncrement) => {
+    if (translateValue <= 0 && Math.sign(translateIncrement) === -1) {
+      return;
+    }
+    setTranslateValue(translateValue + translateIncrement);
+  };
+
   return (
     <>
-      <ul className={styles.carousel}>
-        {dummyData.map((number) => (
-          <li key={number} className={styles.card}>
-            <p className={styles.card__subtitle}>{number}</p>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.controls}>
-        <button className={styles.control__btn}>Prev</button>
-        <button className={styles.control__btn}>Next</button>
+      <div className={styles.row}>
+        <div className={styles.header}>
+          <h3 className={styles.title}>Title</h3>
+          <div className={styles.progressBar}></div>
+        </div>
+      </div>
+      <div className={styles.carousel__container}>
+        <button
+          className={`${styles.handle} ${styles.leftHandle}`}
+          onClick={(e) => handleTranslateChange(e, -translateIncrement)}
+        >
+          <div className={styles.text}>&#8249;</div>
+        </button>
+        <ul
+          ref={ref}
+          style={{ transform: `translateX(-${translateValue}%)` }}
+          className={styles.carousel}
+        >
+          {dummyData.map((number) => (
+            <li key={number} className={styles.card}>
+              <p className={styles.card__subtitle}>{number}</p>
+            </li>
+          ))}
+        </ul>
+        <button
+          className={`${styles.handle} ${styles.rightHandle}`}
+          onClick={(e) => handleTranslateChange(e, translateIncrement)}
+        >
+          <div className={styles.text}>&#8250;</div>
+        </button>
       </div>
     </>
   );
